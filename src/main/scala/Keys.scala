e6abfea7292dbf4490a9848b81f29d10ca87c87c
Erlend Hamnaberg <erlend@hamnaberg.net>
2018-10-12 22:46:07 +0200

    some cleanup

--- src/main/scala/Keys.scala
+++ src/main/scala/Keys.scala
@@ -11,16 +11,11 @@ object Keys {
   val Kotlin = config("kotlin")
   val KotlinInternal = config("kotlin-internal").hide
 
-  val updateCheck = TaskKey[Unit]("update-check", "check for a new version of the plugin")
-  val kotlinCompile = TaskKey[Unit]("kotlin-compile",
-    "runs kotlin compilation, occurs before normal compilation")
-  val kotlincPluginOptions = TaskKey[Seq[String]]("kotlinc-plugin-options",
-    "kotlin compiler plugin options")
-  val kotlinSource = SettingKey[File]("kotlin-source", "kotlin source directory")
-  val kotlinVersion = SettingKey[String]("kotlin-version",
-    "version of kotlin to use for building")
-  val kotlincOptions = TaskKey[Seq[String]]("kotlinc-options",
-    "options to pass to the kotlin compiler")
+  val kotlinCompile = taskKey[Unit]("runs kotlin compilation, occurs before normal compilation")
+  val kotlincPluginOptions = taskKey[Seq[String]]("kotlin compiler plugin options")
+  val kotlinSource = settingKey[File]( "kotlin source directory")
+  val kotlinVersion = settingKey[String]("version of kotlin to use for building")
+  val kotlincOptions = taskKey[Seq[String]]("options to pass to the kotlin compiler")
 
   def kotlinLib(name: String) = sbt.Keys.libraryDependencies +=
     "org.jetbrains.kotlin" % ("kotlin-" + name) % kotlinVersion.value
--- src/main/scala/KotlinPlugin.scala
+++ src/main/scala/KotlinPlugin.scala
@@ -15,33 +15,11 @@ object KotlinPlugin extends AutoPlugin {
 
   override def projectConfigurations = KotlinInternal :: Nil
 
-  override def globalSettings = (onLoad := onLoad.value andThen { s =>
-    Project.runTask(updateCheck in Keys.Kotlin, s).fold(s)(_._1)
-  }) :: Nil
-
   override def projectSettings = Seq(
     libraryDependencies += {
       "org.jetbrains.kotlin" % "kotlin-compiler-embeddable" % kotlinVersion.value % KotlinInternal.name
     },
     managedClasspath in KotlinInternal := Classpaths.managedJars(KotlinInternal, classpathTypes.value, update.value),
-    /*updateCheck in Kotlin := {
-      val log = streams.value.log
-      UpdateChecker("pfn", "sbt-plugins", "kotlin-plugin") {
-        case Left(t) =>
-          log.debug("Failed to load version info: " + t)
-        case Right((versions, current)) =>
-          log.debug("available versions: " + versions)
-          log.debug("current version: " + BuildInfo.version)
-          log.debug("latest version: " + current)
-          if (versions(BuildInfo.version)) {
-            if (BuildInfo.version != current) {
-              log.warn(
-                s"UPDATE: A newer kotlin-plugin is available:" +
-                  s" $current, currently running: ${BuildInfo.version}")
-            }
-          }
-      }
-    },*/
     crossPaths := false,
     kotlinVersion := "1.1.4-3",
     kotlincOptions := Nil,
